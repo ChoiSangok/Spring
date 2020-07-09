738f59bc9dbc6df5098daccd4bce1ad41ee6ff56
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-07-09 21:47:55 +0900

    testmember dto

---
+++ MyBatis_Web/dynamic/dto/TestMember.java
@@ -0,0 +1,34 @@
+package dynamic.dto;
+
+public class TestMember {
+	
+	private String id;
+	private int no;
+	private String pw;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	@Override
+	public String toString() {
+		return "TestMember [id=" + id + ", no=" + no + ", pw=" + pw + "]";
+	}
+	
+	
+	
+
+}
