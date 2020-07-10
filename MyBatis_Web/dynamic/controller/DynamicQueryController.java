f1c9a844977ed849c2bd535fb1151224bf7f7ba4
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-07-10 18:15:46 +0900

    Update DynamicQueryController.java

--- MyBatis_Web/dynamic/controller/DynamicQueryController.java
+++ MyBatis_Web/dynamic/controller/DynamicQueryController.java
@@ -1,6 +1,7 @@
 package dynamic.controller;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -69,10 +70,64 @@ public class DynamicQueryController extends HttpServlet {
 		List res3 = dynamicQueryDao.select3(emp);
 		System.out.println(res3);
 		
+		//--------------------------------------
+		
+		
+		HashMap<String, Object> map= new HashMap<>();
+		map.put("category","ename");
+		map.put("keyword", "FORD");
+		
+		List<Emp> res4 = dynamicQueryDao.select4(map);
+		
+		System.out.println(res4);
+		
+		System.out.println("--------");
+		
+
+		map.put("category","job");
+		map.put("keyword", "SALESMAN");
+		
+		List<Emp> res5 = dynamicQueryDao.select4(map);
+		
+		System.out.println(res5);
+		
+		//----------------------------------------------
+		
+		req.getRequestDispatcher("/WEB-INF/views/dynamic/queryCheckbox.jsp")
+		.forward(req, resp);
+		
+		
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		sqlSession = factory.openSession(true);
+		
+		dynamicQueryDao = sqlSession.getMapper(DynamicQueryDao.class);
+		
+		//전달 파라미터가 하나의 값일 때만 getParameter 를 써야함
+//		String data= req.getParameter("deptno");
+//		System.out.println(data);// 전달 데이터 중 한 개만 꺼내옴
+		
+		//----------------------------------
+		
+		String[] dataArr= req.getParameterValues("deptno");
+		System.out.println(Arrays.toString(dataArr));
+		
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("deptnoArr",dataArr);
+		
+		List<Emp> list = dynamicQueryDao.selectCheckbox(map);
+		
+		for (Emp emp : list) {
+			System.out.println(emp);
+		}
+		
 		
 	}
 
 }
+
 
 
 
