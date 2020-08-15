ede71f28ec668042ba15c679ef0fe0aacd1f276a
SANGOK <tkddhr12345@naver.com>
2020-08-15 21:04:46 +0900

    게[시글 impl

---
+++ Spring04_MVC_PART2/board/service/NoticeServiceImpl.java
@@ -0,0 +1,116 @@
+package com.kh.welcome.board.model.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.kh.welcome.board.model.dao.NoticeDao;
+import com.kh.welcome.board.model.vo.Notice;
+
+import common.util.FileUtil;
+import common.util.Paging;
+
+@Service
+public class NoticeServiceImpl implements NoticeService{
+	
+	@Autowired
+	private NoticeDao noticeDao;
+	
+	/* @Transactional */
+	//스프링에게 트랜잭션 관리를 위임하는 어노테이션
+	//METHOD 또는 CLASS 작성할 수 있다.
+	//CLASS에 지정할 경우, 해당 클래스의 모든 메서드가
+	//스프링에게 트랜잭션 관리를 위임하게 된다.
+	public int insertNotice(Notice notice
+						,List<MultipartFile> files
+						, String root
+			) {
+		
+		int result = noticeDao.insertNotice(notice);
+		
+		//에러 발생을 위한 코드
+		int errorNumber = 10/0;
+		
+		if(!(files.size() == 1
+			&& files.get(0).getOriginalFilename().equals(""))) {
+			
+			//파일업로드를 위해 FileUtil.fileUpload() 호출
+			List<Map<String,String>> filedata 
+					= new FileUtil().fileUpload(files, root);
+			
+			for(Map<String,String> f : filedata) {
+				noticeDao.insertFile(f);
+			}
+		}
+		
+		return result;
+	}
+	
+	public Map<String, Object> selectNoticeList(
+					//현재 페이지
+					 int currentPage
+					//페이지당 노출할 게시글 수
+					,int cntPerPage){
+		
+		 Map<String, Object> commandMap 
+		 		= new HashMap<String, Object>();
+		 //페이징 처리를 위한 객체 생성
+		 Paging p = new Paging(noticeDao.selectContentCnt()
+				 ,currentPage,cntPerPage);
+		 
+		 //현재 페이지에 필요한 게시물 목록
+		 List<Notice> nlist = noticeDao.selectNoticeList(p);
+		 commandMap.put("nlist", nlist);
+		 commandMap.put("paging", p);
+		return commandMap;
+	}
+	
+	//게시물 상세
+	public Map<String,Object> selectNoticeDetail(int nIdx){
+		Map<String,Object> commandMap = new HashMap<String, Object>();
+		
+		Notice notice = noticeDao.selectNoticeDetail(nIdx);
+		List<Map<String,String>> flist = noticeDao.selectFileWithNotice(nIdx);
+		commandMap.put("notice",notice);
+		commandMap.put("flist",flist);
+		return commandMap;
+	}
+	
+	public int deleteFileWithFIdx(int fIdx) {
+		
+		//삭제할 파일의 저장경로를 받아온다.
+		Map<String,String> fileData
+			= noticeDao.selectFileWithFidx(fIdx);
+		
+		//저장경로를 fileUtil.deletFile 메서드의 매개변수로 넘겨서
+		//해당 파일을 삭제한다.
+		FileUtil fileUtil = new FileUtil();
+		fileUtil.deleteFile(fileData.get("savePath"));
+		
+		//파일테이블에서 파일정보를 삭제한다.
+		int res = noticeDao.deleteFileWithFIdx(fIdx);
+		return res;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
