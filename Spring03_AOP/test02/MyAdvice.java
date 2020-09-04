bbe325fb2b8f15eea09401028cb60c0385dafb2d
SANGOK <tkddhr12345@naver.com>
2020-09-04 15:37:12 +0900

    MyAdvice.java

---
+++ Spring03_AOP/test02/MyAdvice.java
@@ -0,0 +1,20 @@
+package test02;
+
+import org.aspectj.lang.JoinPoint;
+
+public class MyAdvice{
+
+	public void before(JoinPoint join) {
+		System.out.println(join.getTarget().getClass());
+		System.out.println(join.getSignature().getName());
+		System.out.println("출석카드를 찍는다");
+	}
+	
+	public void after(JoinPoint join) {
+		System.out.println("집에 간다");
+	}
+	
+	
+	
+
+}
