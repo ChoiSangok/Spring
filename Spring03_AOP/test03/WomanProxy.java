c46f2d92afc5a4b57c6ddb933cb51139010593d7
SANGOK <tkddhr12345@naver.com>
2020-09-07 18:20:12 +0900

    womanproxy

---
+++ Spring03_AOP/test03/WomanProxy.java
@@ -0,0 +1,20 @@
+package test03;
+
+public class WomanProxy implements Develop{
+
+	@Override
+	public void classWork() {
+
+		System.out.println("출석카드를 찍는다");
+		try {
+			new Woman().classWork();
+		} catch (Exception e) {
+			System.out.println("쉬는날이였따");
+			// TODO: handle exception
+		} finally {
+			System.out.println("집에 간다");
+		}
+	}
+
+	
+}
