897747376ec32dd60787a447f49fded25dd0d262
SANGOK <tkddhr12345@naver.com>
2020-09-26 22:44:20 +0900

    실행

---
+++ Spring02_Anno/Run.java
@@ -0,0 +1,22 @@
+package test02;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Run {
+	
+	public static void main(String[] args) {
+//		ApplicationContext factory = new ClassPathXmlApplicationContext("test02/applicationContext.xml");
+//		
+//		System.out.println(factory.getBean("school"));
+		
+		
+		ApplicationContext factory = new ClassPathXmlApplicationContext(
+					"test02/applicationContext.xml"
+				);
+		
+		System.out.println(factory.getBean("mySchool"));
+				
+	}
+
+}
